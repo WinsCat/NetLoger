bdf0bd83e5f3781b473bbdf6e2b3835c1aec170e
wins <cn.lazycat@gmail.com>
2024-09-15 21:25:01 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Copyright (C) 2023-2024 iSeeNEw Studio, Inc. All Rights Reserved 
+#
+# @Time    : 15/9/24 PM9:20
+# @Author  : Wins
+# @Email   : cn.lazycat@gmail.com
+# @File    : main.py
+# @Software: PyCharm
+# @Desc    :
